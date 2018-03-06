b8e2239b948bdf1a7300cb0f95a7d9895e17597a
Matthew Slone <Matthew.Slone@outlook.com>
2018-03-05 22:22:10 -0500

    Allow the user to pick a number not limited to 0 to 100 and continue to propmt the user for a number until the correct hardcoded number has been selected. Format exceptions are built in.

--- GuessMyNumberGame/Program.cs
+++ GuessMyNumberGame/Program.cs
@@ -8,9 +8,58 @@ namespace GuessMyNumberGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+            
+            HandleGuess();
+        }
 
+        static int HandleGuess()
+        {
+            bool quit = false;
+            bool correct = false;
+            int guess = 0;
+            int answerValue = 47;
+
+            try
+            {
+                do
+                {
+                    Console.Write("Guess: ");
+                    guess = int.Parse(Console.ReadLine());
+                    
+                    if (guess < answerValue)
+                    {
+                        Console.WriteLine($"The number is higher than {guess}.");
+                    }
+                    else if (guess > answerValue)
+                    {
+                        Console.WriteLine($"The number is lower than {guess}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"**  Great job!! You guessed it!  **\nThe answer was: {answerValue}");
+                        correct = true;
+                    }
+                }
+                while (!correct && !quit);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid characters. Continue guessing and be sure to enter a " +
+                                  "positive number between 1 and 100.");
+                HandleGuess();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error. Continue guessing and be sure to enter a " +
+                                  "positive number between 1 and 100.");
+                HandleGuess();
+            }
+            return answerValue;
         }
     }
 }
